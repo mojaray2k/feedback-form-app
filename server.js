03b5e5b6415ffc81694d04aeb902e83247c91ea5
Amen Moja Ra <mojaray2k@gmail.com>
2021-04-25 16:20:50 -0400

    added docker file

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const exists = require('fs').exists;
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.json(urlencoded({ extended: false })));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.static('public'));
 app.use('/feedback', express.static('feedback'));
